159ced23c58de491f829352679983853621f5f5d
Benjamin Long <ben@offsite.guru>
2018-06-08 12:17:26 -0400

    Attempt to get working dropdown and fix tip display

--- include/language/en_us.lang.php
+++ include/language/en_us.lang.php
@@ -1012,6 +1012,11 @@ $app_list_strings = array(
         'move' => 'Move',
         'donothing' => 'Do Nothing',
     ),
+    'inboundmail_assign_replies_to_admin' => array(
+        'donothing' => 'Do Nothing',
+        'threadstarter' => 'Thread Starter',
+        'recordowner' => 'Associated Record Owner',
+    ),
 );
 
 $app_strings = array(
