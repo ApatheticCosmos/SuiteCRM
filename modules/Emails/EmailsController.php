52a35cb035821893656a5f9e22d51e632dd9cce7
Benjamin Long <ben@offsite.guru>
2018-07-13 15:22:24 -0400

    Feature/allow users to email using system account as themselves (#13)
    With this option enabled, users will be able to send email out
    of SuiteCRM with their name and email address, but sending though
    the system outgoing email server. Their email address and name
    will be populated in the "From" dropdown.
    Clean up the text, and shorten the LBL_ALLOW_DEFAULT_SELECTION
    label so it doesn't mess with the formatting.
    
    Also change how we get email addresses so we can get every email
    address and reply to that the user has, instead of only the
    primary one. This also sets the reply to correctly.

--- modules/Emails/EmailsController.php
+++ modules/Emails/EmailsController.php
@@ -371,6 +371,7 @@ class EmailsController extends SugarController
     public function action_GetFromFields()
     {
         global $current_user;
+        global $sugar_config;
         $email = new Email();
         $email->email2init();
         $ie = new InboundEmail();
@@ -451,6 +452,31 @@ class EmailsController extends SugarController
 
                 $data[] = $dataAddress;
             }
+        }
+
+        if (isset($sugar_config['email_allow_send_as_user']) && ($sugar_config['email_allow_send_as_user'])) {
+            require_once ('include/SugarEmailAddress/SugarEmailAddress.php');
+            $sugarEmailAddress = new SugarEmailAddress();
+            $userAddressesArr = $sugarEmailAddress->getAddressesByGUID($current_user->id, 'Users');
+            foreach ($userAddressesArr as $userAddress) {
+                $data[] = array(
+                    'type' => 'personal',
+                    'id' => $userAddress['email_address_id'],
+                    'attributes' => array(
+                        'from' => ($userAddress['reply_to_addr'] === '1') ? $current_user->email1 : $userAddress['email_address'],
+                        'reply_to' => $userAddress['email_address'],
+                        'name' => $current_user->full_name,
+                    ),
+                    'prepend' => $prependSignature,
+                    'isPersonalEmailAccount' => true,
+                    'isGroupEmailAccount' => false,
+                    'emailSignatures' => array(
+                        'html' => utf8_encode(html_entity_decode($defaultEmailSignature['signature_html'])),
+                        'plain' => $defaultEmailSignature['signature'],
+                    ),
+                );
+            }
+            unset($userAddress);
         }
 
         $oe = new OutboundEmail();
